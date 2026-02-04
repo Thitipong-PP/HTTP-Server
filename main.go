c6926523c79d28bf41ba619f096a0b1d49c761b2
ThitipongPP <thitipongpp@MacBook-Air-khxng-ThitipongPP.local>
2026-02-04 11:27:38 +0700

    Test Handler

--- main.go
+++ main.go
@@ -6,8 +6,44 @@ import (
 	"net/http"
 )
 
+// Handler
+func testHandler (w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/Test" {
+		http.Error(
+			w, 
+			"404 not found.", 
+			http.StatusNotFound,
+		)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(
+			w,
+			"Method is not supperted.",
+			http.StatusNotFound,
+		)
+		return
+	}
+
+	fmt.Fprintln(w, "Testing Response")
+	fmt.Println("Testing Terminal")
+}
+
+func homeHandler (w http.ResponseWriter, r *http.Request) {
+	fmt.Println("URL : ", r.URL.Path)
+	fmt.Fprintf(w, "Hello this is home handler using net/http")
+}
+
+func statusHandler (w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([] byte("System Normal: All systems operational"))
+}
+
 func main () {
 	// Handle
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/status", statusHandler)
 	http.HandleFunc("/Test", testHandler)
 	
 	// Set Server Port
@@ -16,27 +52,4 @@ func main () {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func testHandler (res http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != "/Test" {
-		http.Error(
-			res, 
-			"404 not found.", 
-			http.StatusNotFound,
-		)
-		return
-	}
-
-	if req.Method != "GET" {
-		http.Error(
-			res,
-			"Method is not supperted.",
-			http.StatusNotFound,
-		)
-		return
-	}
-
-	fmt.Fprintln(res, "Testing Response")
-	fmt.Println("Testing Terminal")
 }
