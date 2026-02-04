0a57cec6841d24ac0259657e9fd6347c4a4a785c
ThitipongPP <thitipongpp@MacBook-Air-khxng-ThitipongPP.local>
2026-02-05 00:07:46 +0700

    Add handle /server/

--- main.go
+++ main.go
@@ -66,6 +66,7 @@ func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/json", jsonHandler)
 	http.HandleFunc("/server", server.ServerHandler)
+	http.HandleFunc("/server/", server.ServerHandler)
 
 	fmt.Println("Starting server at port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
